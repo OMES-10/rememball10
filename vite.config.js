3c1f719f31b020525e680d4f5a1201a4c5e90a98
OMES-10 <mollo078@uottawa.ca>
2026-06-28 14:54:07 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/rememball10/",
+  base: process.env.GITHUB_ACTIONS ? "/rememball10/" : "/",
 });
